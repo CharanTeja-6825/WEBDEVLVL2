d9ff487f87c59b80355473a1dd7e818a18a9e6ab
CharanTeja <rcharanteja2006@gmail.com>
2025-06-25 18:59:28 +0530

    xpress app m2

--- express-app/index.js
+++ express-app/index.js
@@ -8,4 +8,20 @@ const port = 1964;
 
 app.listen(port, function() {
     console.log(`Server Listening on port : ${port}`);
-})
+});
+
+// app.get("/", () => {console.log("this get request is working")});
+
+// testing with app.get() -> this takes two arguments get("/(route or request)", callback => (request, response))
+
+//Example : 
+
+app.get("/ping", (request, response) => {
+    response.send("still running perfectly !!");
+});
+
+// Homepage
+
+app.get("/", (req, res) => {
+    res.send(`<h2 style="color:green;"><u>This is the homepage(root url)</u></h2>`);
+});
