d5906a59c059dcd10765c5429f5617085f0a5002
CharanTeja <rcharanteja2006@gmail.com>
2025-06-25 18:16:43 +0530

    xpress app m1

--- express-app/index.js
+++ express-app/index.js
@@ -1,1 +1,11 @@
-console.log("Jai Balayya !");
+// importing express from node_modules
+
+const express = require("express");
+
+const app = express()
+
+const port = 1964;
+
+app.listen(port, function() {
+    console.log(`Server Listening on port : ${port}`);
+})
